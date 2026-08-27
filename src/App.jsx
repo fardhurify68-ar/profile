a6bb13757506eab802bbe6041560d5f5ec66d673
fardhu <fardhurify68@gmail.com>
2026-08-27 14:46:20 +0700

    implemen component dan props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
 
 function App() {
- 
+
   return (
     <>
-     <p>Fardhu rify</p>
+      <div className=" absolute inset-0 relativ-min h-screen overflow-hidden bg-blue-500200">
+        <h1>PROFILE</h1>
+        <p className="font-bold text-white">Fardhu Rify</p>
+        <img src="juan.jpeg" className="w-32 h-32 rounded-full object-cover"></img>
+        <img src="imo.jpeg" className="w-32 h-32 rounded-full object-cover"></img>
+
+
+        <p><strong>Nama:</strong>fardhu rify</p>
+        <p><strong>Tempat Tanggal Lahir:</strong> Bandar lampung 13 mei 2008</p>
+        <p><strong>Jenis Kelamin:</strong> Laki-laki</p>
+        <p><strong>Alamat:</strong> Bekasi, Jawa Barat</p>
+        <p><strong>Sekolah:</strong> SMK Almuhadjirin Bekasi</p>
+        <p><strong>Kelas:</strong> 12 RPL</p>
+        <p><strong>Jurusan:</strong> Rekayasa Perangkat Lunak</p>
+        <p><strong>Hobi:</strong> Belajar,membaca buku,sholat 5 waktu</p>
+        <p><strong>Cita-cita:masuk RRQ</strong> </p>
+      </div>
+
     </>
-      
+
+
+
+
   );
 }
 
